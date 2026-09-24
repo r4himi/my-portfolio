779e19924372bd97629e0def1b0ab58266a2798f
R4himi <r4himi.10@gmail.com>
2026-09-24 12:01:11 +0430

    Update styles section

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      &copy; {new Date().getFullYear()} <span className={styles.accent}>Samiullah Rahimi</span>
+      &copy;{new Date().getFullYear()} <span className={styles.accent}>Samiullah Rahimi</span> <span className={styles.text}>| All Rights Reserved</span>
     </footer>
   );
 }
